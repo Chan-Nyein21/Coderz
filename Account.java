531ddc1e8bf0deea1bdedf8f0f3f34ae4478278b
Chan-Nyein21 <156650590+Chan-Nyein21@users.noreply.github.com>
2024-04-24 16:05:06 +0700

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,52 @@
+class Account implements BankAccount{
+    private String username;
+    private double balance;
+    private String password;
+
+    public Account(String accountNumber, double initialBalance , String password) {
+        this.username = accountNumber;
+        this.balance = initialBalance;
+        this.password = password;
+    }
+
+    public void setUsername(String accountNumber) {
+        this.username = accountNumber;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    
+    public void withdraw(double amount) throws InsufficientFundsException {
+        if (amount > balance) {
+            throw new InsufficientFundsException("Insufficient funds");
+        }
+        balance -= amount;
+    }
+
+    
+
+}
+
